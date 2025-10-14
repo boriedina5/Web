1ca884cd6aa3055537ec4463a71229be5692a389
Bori Edina <bori.edina.gabriella@blathy.info>
2025-10-14 11:59:11 +0200

    átnézni

--- 2025.10.14/main.js
+++ 2025.10.14/main.js
@@ -1,33 +1,39 @@
 let arrayIngredients = [];
-let receipe = document.getElementsByClassName("Receipes");
-//append
+let imgUrl = document.getElementById("receipePic")
+//let receipe = document.getElementsByClassName("Receipes"); - TMLCollection-t ad vissza, nem egyetlen elemet. A HTMLCollection olyan, mint egy lista (tömbszerű objektum)
+let receipe = document.getElementById("Receipes")
+
+function SpliteElemetToArray(){
+    arrayIngredients = stringIngredients.split(";");
+   /*while(stringIngredients.length){
+        arrayIngredients.push(stringIngredients.splite(";"));
+   } - felesleges*/
+    arrayIngredients = stringIngredients.split(";");
+}
 function createElements(){
+    SpliteElemetToArray()
     //recept div
     let receipeDiv = document.createElement("div")
     receipeDiv.classList.add("receipeDiv")
-    receipe.appendChild(receipeDiv)
+    receipe.appendChild(receipeDiv);
     //cím
     let h2 = document.createElement("h2")
     h2.classList.add("receipeH2");
-    receipeDiv.appendChild("h2")
+    h2.textContent = receipeName.value;
+    receipeDiv.appendChild(h2)
     //kép
-    let img = document.creatElement("img")
+    let img = document.createElement("img")
     img.classList.add("receipeImg")
-    receipeDiv.appendChild("img")
+    img.src = imgUrl.value;
+    receipeDiv.appendChild(img)
     //hozzávalók li
     let li = document.createElement("li")
     li.classList.add("receipeIl")
-    receipeDiv.appendChild("li")
+    receipeDiv.appendChild(li)
+    createIngredientsOl(li)
 }
 
-function SpliteElemetToArray(){
-    let stringIngredients = document.getElementById("receipeIngredients");
-   while(stringIngredients.length){
-        arrayIngredients.push(stringIngredients.splite(";"));
-   }
-}
-
-function createIngredientsOl(){
+function createIngredientsOl(li){
     for(let i = 0; i < arrayIngredients.length; i++){
         let ol = document.createElement("ol")
         ol.innerText = `${arrayIngredients[i]}`
@@ -37,6 +43,4 @@ function createIngredientsOl(){
 
 function addReceipe(){
     createElements();
-    SpliteElemetToArray()
-    createIngredientsOl()
 }
